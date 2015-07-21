ed0cf471647dfa1177befdd012982ae625d503e6
danilomth <danilomatheus72@gmail.com>
2015-07-21 00:37:45 -0300

    Segundo Commit Atualização

--- js/init.js
+++ js/init.js
@@ -13,6 +13,17 @@
           accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
         });
       });
+        $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+        // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
+       $(document).ready(function(){
+    $('ul.tabs').tabs();
+  });
            
       
         
